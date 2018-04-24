70ac93ffd2737419831b241feffc16f0655e8a28
Nolan McCafferty <njma2016@pomona.edu>
2018-04-23 20:29:33 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php 
-	include_once("application/controller/controller.php");
-  
-	$controller = new Controller();
-	$controller->invoke();
+session_start();
 ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+<title>Coop Fountain Online</title>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<script language="javascript"></script></head>
+
+
+<body>
+
+<div class="w3-container w3-jumbo w3-blue">Coop Fountain Online</div><br>
+
+<img src="public/img/cecil.jpg">
+<img src="public/img/coop.jpg" style="float:right; padding: 30px; width:45%;"><br><br>
+<br><center><a class="w3-btn w3-large w3-xxlarge w3-hover-grey w3-orange" href="order.php">Ready to Order!</a></center>
+</body>
+</html>
